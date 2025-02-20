f79531261dd7f61eeb7a308042e1eaf0598b243f
Yizbin <abrahamcbringas@gmail.com>
2025-02-20 14:39:15 -0600

    Se crearon el iniciar sesion

---
+++ clinicaPresentacion/src/main/java/com/mycompany/clinicapresentacion/ClinicaPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clinicapresentacion;
+
+/**
+ *
+ * @author PC Gamer
+ */
+public class ClinicaPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
